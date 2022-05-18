779d378aa61a9474bca78ec6cade22ecd5ebeb35
Meng <57026429+jiangxi-am@users.noreply.github.com>
2022-05-18 23:08:57 +0800

    Update course_data.js

--- course_data.js
+++ course_data.js
@@ -1,4 +1,4 @@
-var course_data = {"result":{"id":"13","title":"2022年第13期","uri":"https://h5.cyol.com/special/daxuexi/cd5nkg1yo6/index.html"},"status":200}
+var course_data = {"result":{"id":"13","title":"“青年大学习”2022年特辑","uri":"https://h5.cyol.com/special/daxuexi/cd5nkg1yo6/index.html"},"status":200}
 // {
 //     "result": {
 //       "id": "13",
