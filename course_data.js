17457a0ff1783467c702e9f77b3aed6a543c1893
Meng <57026429+jiangxi-am@users.noreply.github.com>
2022-05-18 22:54:16 +0800

    Add files via upload

---
+++ course_data.js
@@ -0,0 +1,9 @@
+var course_data = {"result":{"id":"13","title":"2022年第13期","uri":"https://h5.cyol.com/special/daxuexi/cd5nkg1yo6/index.html"},"status":200}
+// {
+//     "result": {
+//       "id": "13",
+//       "title": "“青年大学习”2022年特辑",
+//       "uri": "https://h5.cyol.com/special/daxuexi/cd5nkg1yo6/index.html"
+//     },
+//     "status": 200
+//   }
